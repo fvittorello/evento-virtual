4c0781de7639f8d06d6529328e2c7cf33a5104d6
Fer Vittorello <fvittorello@gmail.com>
2021-01-07 18:56:37 -0300

    feat(Description) add component and missing assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from 'components/Header';
+import { MainContent } from 'components/MainContent';
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<MainContent />
 		</div>
 	);
 }
