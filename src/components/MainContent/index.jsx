3a74c3761cc29a8645e91861b7e0fdb8f8e2561c
Fer Vittorello <fvittorello@gmail.com>
2021-01-07 16:49:39 -0300

    feat(MainContent) add component & basic styles

---
+++ src/components/MainContent/index.jsx
@@ -0,0 +1,13 @@
+// Components
+import { Description } from 'components/Description';
+
+// Styles
+import './mainContent.styles.scss';
+
+export const MainContent = () => {
+	return (
+		<div className='main-content'>
+			<Description />
+		</div>
+	);
+};
