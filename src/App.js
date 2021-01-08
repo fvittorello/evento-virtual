795cb68c70e2fedd201018c13be9415f486fbc33
Fer Vittorello <fvittorello@gmail.com>
2021-01-07 21:06:30 -0300

    feat(Footer) add Footer and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Header } from 'components/Header';
 import { MainContent } from 'components/MainContent';
+import { Footer } from 'components/Footer';
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<MainContent />
+			<Footer />
 		</div>
 	);
 }
--- src/components/SocialList/index.jsx
+++ src/components/SocialList/index.jsx
@@ -2,46 +2,49 @@
 import './socialList.styles.scss';
 
 // Icons
+import { IconContext } from 'react-icons';
 import { FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram, FaSnapchatGhost, FaYoutube } from 'react-icons/fa';
 
 export const SocialList = () => {
 	return (
-		<ul className='social-media'>
-			<li>
-				<a className='social-media__link' href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
-					<FaTwitter size={24} color='white' />
-				</a>
-			</li>
+		<IconContext.Provider value={{ size: 24, color: 'white' }}>
+			<ul className='social-media'>
+				<li>
+					<a className='social-media__link' href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
+						<FaTwitter />
+					</a>
+				</li>
 
-			<li>
-				<a className='social-media__link' href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
-					<FaFacebookF size={24} color='white' />
-				</a>
-			</li>
+				<li>
+					<a className='social-media__link' href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+						<FaFacebookF />
+					</a>
+				</li>
 
-			<li>
-				<a className='social-media__link' href='https://linkedin.com' target='_blank' rel='noopener noreferrer'>
-					<FaLinkedinIn size={24} color='white' />
-				</a>
-			</li>
+				<li>
+					<a className='social-media__link' href='https://linkedin.com' target='_blank' rel='noopener noreferrer'>
+						<FaLinkedinIn />
+					</a>
+				</li>
 
-			<li>
-				<a className='social-media__link' href='https://youtube.com' target='_blank' rel='noopener noreferrer'>
-					<FaYoutube size={24} color='white' />
-				</a>
-			</li>
+				<li>
+					<a className='social-media__link' href='https://youtube.com' target='_blank' rel='noopener noreferrer'>
+						<FaYoutube />
+					</a>
+				</li>
 
-			<li>
-				<a className='social-media__link' href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
-					<FaInstagram size={24} color='white' />
-				</a>
-			</li>
+				<li>
+					<a className='social-media__link' href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+						<FaInstagram />
+					</a>
+				</li>
 
-			<li>
-				<a className='social-media__link' href='https://snapshat.com' target='_blank' rel='noopener noreferrer'>
-					<FaSnapchatGhost size={24} color='white' />
-				</a>
-			</li>
-		</ul>
+				<li>
+					<a className='social-media__link' href='https://snapshat.com' target='_blank' rel='noopener noreferrer'>
+						<FaSnapchatGhost />
+					</a>
+				</li>
+			</ul>
+		</IconContext.Provider>
 	);
 };
