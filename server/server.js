1cf61417a199a0ec7eb4dccbf6fae67928202d98
Fer Vittorello <fvittorello@gmail.com>
2021-01-09 15:46:22 -0300

    feat(db) create and test db connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
 const app = express();
-const dotenv = require('dotenv');
-
-// Settings
-dotenv.config();
 
 app.listen('5050', () => {
 	const date = new Date();
