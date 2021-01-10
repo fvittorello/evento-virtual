7b3458cbadcdbaac0022980e5c2262218b46571e
Fer Vittorello <fvittorello@gmail.com>
2021-01-10 15:19:46 -0300

    chore(Header) change background image

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,8 +1,12 @@
+// Assets
+import headerBg from "assets/header-background.svg";
+
+// Styles
 import './header.styles.scss';
 
 export const Header = () => {
 	return (
-		<div className='header'>
+		<div className='header' style={{background: `#ffd6a8 url(${headerBg}) center no-repeat`}}>
 			<span className='header__category'>Webinar</span>
 			<h2 className='header__title'>El reto de humanizar el CX financiero en 2021.</h2>
 			<h2 className='header__subtitle'>La experiencia de un Unicornio de Latam</h2>
